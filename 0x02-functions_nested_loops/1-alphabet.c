955ff60afea70cc599f46f3fa35ed89598bfa50f
Beatrice <ushiksmandi@gmail.com>
2022-09-13 13:26:23 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - No entry point
+ * Return: Always 0 (success!)
+ */
+
+void print_alphabet(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		_putchar(al);
+	}
+	_putchar('\n');
+	return (0);
+}
