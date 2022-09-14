8d3d122ac534cef0aca29474480dbeff9615d83e
Beatrice <ushiksmandi@gmail.com>
2022-09-13 17:15:00 -0700

    final

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,17 +9,18 @@
 
 int print_last_digit(int n)
 {
-	int last;
+	int last = n % 10;
 
-	if (n > 0)
+	if (n < 0)
 	{
-		last = n % 10;
-		_putchar(last + '0');
+		n = n * -1;
+
+		_putchar(n % 10 + '0');
+		return (n % 10);
 	}
 	else
 	{
-		last = -1 * n % 10;
 		_putchar(last + '0');
+		return (last % 10);
 	}
-	return (last % 10);
 }
