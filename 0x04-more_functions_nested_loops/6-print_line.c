bacff9cd3fcf44873bff7670aa9162b11b7b6166
Beatrice <ushiksmandi@gmail.com>
2022-09-15 13:00:19 -0700

    nests

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - check main
+ * @n: an integer input
+ * Description: function that draws a straight line
+ * Return: Nothing
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
