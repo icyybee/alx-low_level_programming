c43ca891c966c25507fe26073544f8b59e3b525b
Beatrice <ushiksmandi@gmail.com>
2022-09-19 16:32:47 -0700

    tests

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -4,7 +4,6 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * Return: Always 1 success
- * On error -1 is returned, and error is set appropriately
  */
 
 int _putchar(char c)
