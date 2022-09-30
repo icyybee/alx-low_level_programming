0fab014088c97359c01ba7e9a3b48b9741e7da03
Beatrice <ushiksmandi@gmail.com>
2022-09-30 14:14:33 -0700

    pushing p

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints its name
+ * @argc: the number of arguments passed
+ * @argv: arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
